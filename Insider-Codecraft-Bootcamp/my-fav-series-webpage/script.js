8efec3276f903da59d5a727918385a0068dca1df
Selvinaz Zeynep Kıyıkcı <132915659+selvikiyikci@users.noreply.github.com>
2025-05-27 21:09:59 +0300

    Delete Insider-Codecraft-Bootcamp directory

--- Insider-Codecraft-Bootcamp/my-fav-series-webpage/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.querySelector(".hamburger");
-    const sidebar = document.querySelector(".sidebar");
-
-    menuToggle.addEventListener("click", function() {
-        sidebar.classList.toggle("active");
-    });
-});
